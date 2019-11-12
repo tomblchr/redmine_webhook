2a1e75ccd6bbe7a4c02feaed6772207ac185c6e6
Tom Belcher <git@blchr.com>
2019-11-11 21:46:23 -0500

    Only include version if it is not empty

--- lib/redmine_webhook/version_wrapper.rb
+++ lib/redmine_webhook/version_wrapper.rb
@@ -1,15 +1,16 @@
 module RedmineWebhook
-    class VersionWrapper
-      def initialize(version)
-        @version = version
-      end
-  
-      def to_hash
-        {
-          :id => @version.id,
-          :name => @version.name
-        }
-      end
+  class VersionWrapper
+    def initialize(version)
+      @version = version
+    end
+ 
+    def to_hash
+      return nil unless @version
+      {
+        :id => @version.id,
+        :name => @version.name
+      }
     end
   end
+end
   
